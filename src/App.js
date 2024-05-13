999bf85b07e7d9364d07a5de8e2ed3a0bf8e575b
Troy Brewer <troy.brewer@alliedsolutions.net>
2024-05-13 09:59:01 -0400

    Move to public repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Plot from "react-plotly.js";
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState({grid:null});
+  const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT
+
+  useEffect(() => {
+    // fetch('https://ri8x2qu7mg.execute-api.us-east-2.amazonaws.com/')
+    fetch(`${API_ENDPOINT}`)
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Plot
+        data={[
+          {
+            z: data.grid,
+            colorscale: "Portland",
+          
+            mode: "markers",
+            type: "surface",
+          },
+        ]}
+        layout={{
+          title: `Lowest Point: ${data.lowest}\nStarting Point:${data.starting}`,
+        }}
+        style={{ width: "100%", height: "100%" }}
+      />
     </div>
   );
 }
